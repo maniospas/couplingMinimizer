f65ed3b0d6ea4993bd39efbd0c7c81e28f35f794
mkrasanakis <maniospas@hotmail.com>
2019-04-17 18:14:24 +0300

    added typehints to conductance

--- coupling.py
+++ coupling.py
@@ -19,11 +19,11 @@ seed_vector = {u: 1 if u in module else 0 for u in G.nodes()}
 ranks = nx.pagerank(G, alpha=0.85, personalization=seed_vector, weight="weight")
 # calculate the minimum conductance
 unpersonalized_ranks = nx.pagerank(G, alpha=0.85, weight="weight")
-ranks = {u: ranks[u]/unpersonalized_ranks[u] if unpersonalized_ranks[u]!=0 else 0 for u in G.nodes()}
-print(len({u: ranks[u] for u in ranks.keys() if ranks[u]!=1}))
+ranks = {u: ranks[u]/unpersonalized_ranks[u] if unpersonalized_ranks[u] != 0 else 0 for u in G.nodes()}
+print(len({u: ranks[u] for u in ranks.keys() if ranks[u] != 1}))
 best_cond, best_set = conductance.sweep(ranks, G)
 
 print("Original conductance",conductance.conductance(seed_vector, G))
 print("New conductance", conductance.conductance({u: 1 if u in best_set else 0 for u in G.nodes()}, G))
 
-vis.visualize(G, ranks, {u: 1 if u in module else 0 for u in G.nodes()})
+vis.visualize(G, ranks, {u: 1 if u in best_set else 0 for u in G.nodes()})
